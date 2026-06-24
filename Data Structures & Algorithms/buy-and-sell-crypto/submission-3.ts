3118c066a665464728929969ba71ab20f6969d99
Alex Metelli <alex-metelli@gmx.com>
2026-06-24 08:43:40 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let l = 100;
+        let li = 0;
+        let h = 0;
+        let hi = 100;
+        let b = 0;
+        let s = prices.length - 1;
+        while (s >= 0) {
+            const pb = prices[b];
+            const ps = prices[s]
+            console.log('l', l);
+            console.log('h', h);
+            if ( pb < l && b < hi) {l = pb; li = b};
+            if ( ps > h && s > li) {h = ps; hi = s};
+            b++;
+            s--;
+        }
+        console.log('l', l);
+        console.log('h', h);
+        const r = h > l ? h - l : 0;
+        return r;
+    }
+}
