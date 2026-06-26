87d979f52860bce5489cefa121913a063a80acce
Alex Metelli <alex-metelli@gmx.com>
2026-06-27 07:57:21 +0800

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1: string, s2: string): boolean {
+        if (s2.length === 1 && s1 === s2) return true;
+        const l = s1.length;
+        let i = 0;
+        const s = s1.split('').sort().join('');
+        console.log('s', s);
+        while (i <= s2.length - l) {
+            let sub = s2.slice(i, i + l).split('').sort().join('');
+            console.log('sub', sub);
+            if (sub === s) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
