9570be5cfdfd4942f83e0d02174622cd33b4d788
Alex Metelli <alex-metelli@gmx.com>
2026-06-27 15:38:29 +0800

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while(l <= r) {
+            let k = Math.floor((l + r) / 2);
+            let e = nums[k];
+            if (e === target) {
+                return k 
+            }else if (e < target) {
+                l = k + 1;
+            } else {
+                r = k - 1;
+            }
+        }
+        return - 1;
+    }
+}
