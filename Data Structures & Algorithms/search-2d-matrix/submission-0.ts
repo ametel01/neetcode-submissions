be9df2f62d72d88c663224183d69ce3e8b0b01e0
Alex Metelli <alex-metelli@gmx.com>
2026-06-27 16:37:51 +0800

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        const l = matrix[0].length;
+        let r = 0;
+
+        for (let i = 0; i < matrix.length; i++) {
+            if (matrix[i].includes(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
