852c0747324c5c164016f5b41527fa5546a8f415
Alex Metelli <alex-metelli@gmx.com>
2026-06-28 12:33:50 +0800

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        const out: number[] = [];
+        let i = 0;
+        let j = 1;
+
+
+        const stack: number[] = [];
+        while (i < temperatures.length) {
+            if (temperatures[j] <= temperatures[i]) {
+                stack.push(temperatures[j]);
+                j++
+            } else if (j === temperatures.length) {
+                out.push(0);
+                stack.length = 0;
+                i++;
+                j = i + 1;
+            } else {
+                i++;
+                j = i + 1;
+                out.push(stack.length + 1);
+                stack.length = 0;
+            }
+        }
+        return out;
+    }
+}
